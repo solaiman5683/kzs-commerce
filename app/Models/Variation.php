860207aa1a300fd88d4bee430a980e3f3597a17f
Md.Shariful islam <sharif.net888@gmail.com>
2023-12-13 16:22:19 +0600

    rgdr

---
+++ app/Models/Variation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Variation extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);;
+    }
+}
